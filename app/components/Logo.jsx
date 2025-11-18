641d416aeb9ddd86b4d0b56587af0cfd028ce3c4
Knightowl00 <blazeboxdev@protonmail.com>
2025-11-18 14:23:35 -0500

    added main scafolds

---
+++ app/components/Logo.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="my-4 w-100 h-[800px] absolute top-[-150px] flex items-center justify-center ">
+      <Image
+        src="/m-logo.png"
+        alt="Transit M Logo"
+        fill
+        className="object-contain"
+        sizes="(max-width: 768px) 100vw, 128px"
+        priority
+      />
+    </div>
+  );
+}
